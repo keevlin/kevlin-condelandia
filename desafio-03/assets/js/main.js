b6eff14c5385d36e2dec47785e4116b206a1daed
keevlin <kevlin_cb@hotmail.com>
2022-04-01 20:28:57 -0300

    :package:

---
+++ desafio-03/assets/js/main.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    reset: true
+  });
+  
+  ScrollReveal().reveal('.shop', { delay: 200 });
+  ScrollReveal().reveal('.date', { delay: 400 });
+  ScrollReveal().reveal('.title', { delay: 500 });
+  ScrollReveal().reveal('.subtitle', { delay: 500 });
