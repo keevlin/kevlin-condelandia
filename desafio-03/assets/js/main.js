e3da072f631547984300629b7cf1352593428a45
keevlin <kevlin_cb@hotmail.com>
2022-04-03 20:38:49 -0300

    :package: desafio 3 finalizado

--- desafio-03/assets/js/main.js
+++ desafio-03/assets/js/main.js
@@ -1,11 +1,15 @@
 const sr = ScrollReveal({
-    origin: 'top',
-    distance: '50px',
-    duration: 2000,
-    reset: true
-  });
-  
-  ScrollReveal().reveal('.shop', { delay: 200 });
-  ScrollReveal().reveal('.date', { delay: 400 });
-  ScrollReveal().reveal('.title', { delay: 500 });
-  ScrollReveal().reveal('.subtitle', { delay: 500 });
+  origin: 'top',
+  distance: '50px',
+  duration: 2000,
+  reset: true
+});
+
+
+
+ScrollReveal().reveal('.card', { delay: 200 });
+ScrollReveal().reveal('.card__img',{delay:300})
+ScrollReveal().reveal('.card__conteudo', { delay: 400 });
+ScrollReveal().reveal('.card__texto', { delay: 500 });
+
+
